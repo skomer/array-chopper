f015749b12343692e21c9fb174802b50ddb64235
Jo <parusmontanus@gmail.com>
2017-10-27 14:58:17 +0100

    Passing test: returns expected array when integer even

--- chopper.js
+++ chopper.js
@@ -3,13 +3,28 @@ var Chopper = function() {}
 Chopper.prototype = {
 
 	chop: function(inputArray, integerN) {
-
 		outputArray = [];
+		remainder = inputArray.length % integerN;
 
-		for(i = 0; i < integerN; i++) {
-			outputArray.push(inputArray[i]);
+		if (remainder === 0) {
+			quantityInEachSubArray = inputArray.length / integerN;
+
+			for (i = 0; i < integerN; i++) {
+				subArray = [];
+
+				for (j = 0; j < quantityInEachSubArray; j++) {
+					desiredIndex = (quantityInEachSubArray * i) + j;
+					subArray.push(inputArray[desiredIndex]);
+				}
+
+				outputArray.push(subArray);
+			}
 		}
-
+		else {
+			for(i = 0; i < integerN; i++) {
+				outputArray.push(inputArray[i]);
+			}
+		}
 
 		return outputArray;
 	}
