f050131a5832dae93ba0c97b12a31a234b4a520b
Jo <parusmontanus@gmail.com>
2017-10-27 15:05:58 +0100

    Passing test: returns the expected array when there is a remainder

--- chopper.js
+++ chopper.js
@@ -21,9 +21,28 @@ Chopper.prototype = {
 			}
 		}
 		else {
-			for(i = 0; i < integerN; i++) {
-				outputArray.push(inputArray[i]);
+			quantityInRemainderSubArray = Math.round(remainder / 2);
+			quantityInEachSubArray = (inputArray.length - quantityInRemainderSubArray) / (integerN - 1);
+
+			for (i = 0; i < (integerN - 1); i++) {
+				subArray = [];
+
+				for (j = 0; j < quantityInEachSubArray; j++) {
+					desiredIndex = (quantityInEachSubArray * i) + j;
+					subArray.push(inputArray[desiredIndex]);
+				}
+
+				outputArray.push(subArray);
 			}
+
+			remainderSubArray = [];
+
+			for (k = quantityInRemainderSubArray; k > 0; k--) {
+				desiredIndex = inputArray.length - k;
+				remainderSubArray.push(inputArray[desiredIndex]);
+			}
+
+			outputArray.push(remainderSubArray);
 		}
 
 		return outputArray;
